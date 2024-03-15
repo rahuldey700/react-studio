225a0ab131542fe0554075f417e719e45fc50871
rahuldey700 <116687471+rahuldey700@users.noreply.github.com>
2024-03-15 12:26:19 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import bakeryItem from './components/BakeryItem.js';
 import bakeryData from "./assets/bakery-data.json";
 
+
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
-bakeryData.forEach((item) => {
-  item.image = process.env.PUBLIC_URL + "/" + item.image;
+bakeryData.forEach((bakeryItem) => {
+  bakeryItem.image = process.env.PUBLIC_URL + "/" + bakeryItem.image;
 });
 /* ############################################################## */
 
@@ -15,21 +17,60 @@ function App() {
   const [data, setData] = useState(bakeryData);
   const [cart, setCart] = useState([]); 
 
+  const loadData = () => {
+    setData([...bakeryData]);
+  }
 
-  return (
-    <div className="App">
-      <h1>Rahul's Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+  useEffect(() => {
+    console.log('component loaded', data.length)
+    loadData();
+  }, []) 
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+  useEffect(() => {
+    console.log('cart changed: ', cart.length)
+    console.log('data: ', data.length)
+  }, [cart, data])
 
-      <div>
-        <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
+  const addToCart = (bakeryItem) => {
+    console.log('adding to cart:', bakeryItem)
+
+    setCart(prev_cart =>
+      [...prev_cart, bakeryItem]
+        .filter(price => bakeryItem.price < 5))
+  }
+
+  const bakeryItemsJSX = data.map((bakeryItem, index) => (
+    <div key={index}>
+      <p>{`Bakery Item ${index}, ${bakeryItem.description}, ${bakeryItem.price}`}</p>
+      <button onClick={() => addToCart(bakeryItem)}>Add to Cart</button>
     </div>
-  );
+  ));
+
+const cartJSX = cart.length === 0 ? <p>Cart is empty</p>
+: cart.map((item, index) => <p key={index}>{item.name}: $ {item.price}</p>)
+
+return (
+  <div className="App">
+    <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      {cartJSX}
+    {bakeryItemsJSX}
+
+    <button onClick={() => {
+      console.log('filtering data')
+      setData(prev_data => prev_data.filter((bakeryItem, index) => index % 2 === 0))
+    }}>filter</button>
+
+    <button onClick={() => {
+      console.log('restoring data')
+      setData(bakeryData) // original data
+    }}>clear</button>
+
+    <div>
+      <h2>Cart</h2>
+      {/* TODO: render a list of items in the cart */}
+    </div>
+  </div>
+);
 }
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const BakeryItem = ({ item }) => {
     return (
-        <div className="bakery-item">
+        <div className="BakeryItem">
             <h2>{item.name}</h2>
             <p>{item.description}</p>
             <p>{item.price}</p>
